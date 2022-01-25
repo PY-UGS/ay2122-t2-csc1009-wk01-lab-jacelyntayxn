6302486d8ecb826c2b6b5d46d7984175bac711c8
jacelyntayxn <94168656+jacelyntayxn@users.noreply.github.com>
2022-01-25 16:21:45 +0800

    Add files via upload

---
+++ Wk1Q2a.java
@@ -0,0 +1,7 @@
+public class Wk1Q2a
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello, I am Jacelyn!");
+    }
+}
---
+++ Wk1Q2b.java
@@ -0,0 +1,14 @@
+public class Wk1Q2b
+{
+    public static void main(String arg[])
+    {
+        String currency="CSC1009";
+        switch(currency)
+        {
+            case "CSC1009":
+                System.out.println("Object-Oriented Programming");
+                break;
+            default:
+        }
+    }
+}
